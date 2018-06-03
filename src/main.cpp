24f8d50a7329b7edf16c20046716718f29cf939c
duser <lachlan-27@hotmail.com>
2018-06-03 01:58:21 -0700

    minor fixes, screenshot

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(-0.2,0.00,-10.00);
+  pid.Init(-0.21,0.00,-9.00);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -60,10 +60,10 @@ int main()
           */
           pid.UpdateError(cte);
           double err = pid.TotalError();
-          // uncomment for on-line twiddling...
-          // pid.Twiddle();
+          // for on-line twiddling...
+          pid.Twiddle();
           double correction = pid.Calculate();
-          steer_value = pid.Calculate();
+          steer_value = correction;
 
           if (steer_value>1) {steer_value = 1;}
           if (steer_value<-1) {steer_value = -1;}
